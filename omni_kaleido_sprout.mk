8c57600f41c49fc75009b02070ccb56a31001bd6
Enceka <enceka@yeah.net>
2022-04-26 09:06:26 +0800

    first version

---
+++ omni_kaleido_sprout.mk
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2022 The Android Open Source Project
+# Copyright (C) 2022 SebaUbuntu's TWRP device tree generator
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# Inherit from kaleido_sprout device
+$(call inherit-product, device/sharp/kaleido_sprout/device.mk)
+
+# Inherit some common Omni stuff.
+$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := kaleido_sprout
+PRODUCT_NAME := omni_kaleido_sprout
+PRODUCT_BRAND := SG
+PRODUCT_MODEL := S1
+PRODUCT_MANUFACTURER := sharp
